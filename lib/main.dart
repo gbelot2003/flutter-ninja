89c7eb0f532c6088d560881987a3f88acbf67cae
Gerardo Belot <gerardo.belot@gmail.com>
2022-12-16 01:49:23 -0600

    Agregando imagenes y dividers para el id

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,23 @@ class NinjaCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Center(
+              child: CircleAvatar(
+                radius: 40,
+                backgroundImage: NetworkImage(
+                    'https://www.giantbomb.com/a/uploads/scale_small/12/129604/2862389-tumblr_nwlw7bccic1tkay0no1_1280.png'),
+              ),
+            ),
+            Divider(
+              height: 90,
+              color: Colors.grey[800],
+            ),
             Text(
               'NAME',
               style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10,
             ),
             Text(
               'Chun-Li',
@@ -29,6 +43,45 @@ class NinjaCard extends StatelessWidget {
                   letterSpacing: 2.0,
                   fontSize: 28.0,
                   fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'Current Ninja Level',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '8',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "chun@hotmail.com",
+                  style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 16,
+                      letterSpacing: 1.0),
+                ),
+              ],
             ),
           ],
         ),
